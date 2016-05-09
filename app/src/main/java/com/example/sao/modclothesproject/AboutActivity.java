8a137106443af64208b12af6ad0349dc169516dc
PariPear <pare_saai@hotmail.com>
2016-05-10 06:25:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/sao/modclothesproject/AboutActivity.java
@@ -0,0 +1,41 @@
+package com.example.sao.modclothesproject;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class AboutActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        addListenerOnButton();
+
+    }
+
+    public void addListenerOnButton() {
+
+        button = (Button) findViewById(R.id.button1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.facebook.com/modclothesapp"));
+                startActivity(browserIntent);
+
+            }
+
+        });
+
+    }
+
+}
